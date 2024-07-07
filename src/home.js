ad794e75f0a9dd786a27370ffdaaf3eb56a3c588
Armin <160379151+ArminKGW@users.noreply.github.com>
2024-07-07 15:14:39 +0200

    Add Homepage

---
+++ src/home.js
@@ -0,0 +1,29 @@
+const elements = (function(){
+    const content = document.querySelector("#content");
+    return {content};
+})();
+
+function makeDiv(parent){
+    const element = document.createElement("div");
+    parent.appendChild(element);
+    return element;
+}
+
+function display(){
+    const container = elements.content;
+
+    const name = makeDiv(container);
+    name.classList.add("name");
+    name.textContent = "Baratie";
+
+    const headline = makeDiv(container);
+    headline.classList.add("headline");
+    headline.textContent = "Dive into Delectable Depths at Baratie";
+    
+    const text = makeDiv(container);
+    text.classList.add("text");
+    text.textContent = "Experience a world of flavor with our fresh, sustainably sourced seafood. Enjoy exquisite dishes in a serene, coastal-inspired setting, perfect for any occasion. Immerse yourself in exceptional service and culinary artistry that transforms every meal into a masterpiece.";
+}
+
+export default display;
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("working");
+import displayHome from "./home.js";
+
+displayHome();
