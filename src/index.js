19554d6585300f08892246dcf0311bc6b3080b71
Armin <160379151+ArminKGW@users.noreply.github.com>
2024-07-09 00:34:36 +0200

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import displayHome from "./home.js";
 import displayMenu from "./menu.js";
 
-displayMenu();
+displayHome();
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("home")){
+        clearPage();
+        displayHome();
+    }
+    if(e.target.classList.contains("menu")){
+        clearPage();
+        displayMenu();
+    }
+    if(e.target.classList.contains("contact")){
+        clearPage();
+        displayContact();
+    }
+});
+
+function clearPage(){
+    const content = document.querySelector("#content");
+    while(content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+}
